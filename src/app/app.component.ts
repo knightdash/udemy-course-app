172d89ec2d8b1b1b46c5b95f644549a6122ed0d5
DashBatdorj <dash.b@novelsoft.mn>
2023-02-01 13:53:13 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
+//////// ********* Section2 : Basic **************** ////////
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   // styleUrls: ['./app.component.css']
+//   styles: [`
+//     h3 {
+//       color: dodgerblue;
+//     }
+//   `],
+// })
+// export class AppComponent {
+// }
+
+
+
+
+
+
+//////// ********* Section5 : Components & Databinding Deep Dive**************** ////////
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [],
 })
 export class AppComponent {
-  title = 'order-app';
+  serverElements = [];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,56 @@
+//////// ********* Section2 : Basic **************** ////////
+
+// import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// import { ServerComponent } from './Section2-Basic/server/server.component';
+// import { ServersComponent } from './Section2-Basic/servers/servers.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     ServerComponent,
+//     ServersComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     FormsModule,
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////// ********* Section5 : Components & Databinding Deep Dive**************** ////////
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './Section5-Component&DatabindingDeep/cockpit/cockpit.component';
+import { ServerElementComponent } from './Section5-Component&DatabindingDeep/server-element/server-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockpitComponent,
+    ServerElementComponent,  
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
