6b33c72c3d97c6461bbfc1851c655d3f9673d97e
knightdash <batdorj999@gmail.com>
2023-03-19 23:23:02 +0800

    update

--- src/app/Section18-Making Http Requests/auth-interceptor.service.ts
+++ src/app/Section18-Making Http Requests/auth-interceptor.service.ts
@@ -1,5 +1,4 @@
-import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { tap } from "rxjs";
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
--- src/app/Section18-Making Http Requests/logging-interceptor.service.ts
+++ src/app/Section18-Making Http Requests/logging-interceptor.service.ts
@@ -1,5 +1,5 @@
-import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable, tap } from "rxjs";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 export class LoggingInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
